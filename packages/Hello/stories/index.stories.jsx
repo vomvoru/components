3c95987a70962d2ade663b7c00373975b474857d
vomvoru <qkrcjfgus3@gmail.com>
2019-05-16 01:52:55 +0900

    docgen 적용

--- packages/Hello/stories/index.stories.jsx
+++ packages/Hello/stories/index.stories.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { withInfo } from '@storybook/addon-info';
 
 import Sample from '../src/index';
 
 export default storiesOf => {
   storiesOf('Button123', module)
-    .add('with text', () => <Sample>Hello Button</Sample>)
-    .add('with emoji', () => (
-      <Sample>
-        <span role="img" aria-label="so cool">
-          😀 😎 👍 💯
-        </span>
-      </Sample>
-    ));
+    .add('with text', withInfo({ inline: true })(() => <Sample>Hello Button</Sample>))
+    .add(
+      'with emoji',
+      withInfo({ inline: true })(() => (
+        <Sample>
+          <span role="img" aria-label="so cool">
+            😀 😎 👍 💯
+          </span>
+        </Sample>
+      ))
+    );
 };
